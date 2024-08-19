02612ff7e9972adab1af9a17bed4cbb068836e7f
starrieskye <skyekerst04@gmail.com>
2024-08-19 11:49:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
+
+    
+    function mouseStopped() {
+        cursor.style.display = "none";
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 5000);
+});
+
+
+document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+});
